06bd3f34b8353b0f10aa925fef9cef6ac55187d0
Mesut MUTLU <mesut_mutlu83@yahoo.fr>
2018-05-23 14:25:21 +0300

    Initial commit

---
+++ EndToEndProcess/04_Select_Train_Model/13_Fit_Predict_Evaluate_Using_CV.py
@@ -0,0 +1,3 @@
+#with whole training set
+#with whole training set using CV
+#do not forget about ensemble methods (existings or custom created using voting, bagging, pasting, out-of-bag, boosting, stacking, etc.
---
+++ EndToEndProcess/05_Fine_Tune_Model/14_Tune_HyperParameters.py
@@ -0,0 +1,2 @@
+#using gridsearch
+#using randomsearch
