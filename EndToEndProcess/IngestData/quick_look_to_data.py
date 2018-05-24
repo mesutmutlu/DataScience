920ab0d837fe19e08cd7b9ab348d66f67e9480c4
Mesut MUTLU <mesut_mutlu83@yahoo.fr>
2018-05-24 16:51:22 +0300

    -

--- EndToEndProcess/IngestData/quick_look_to_data.py
+++ EndToEndProcess/IngestData/quick_look_to_data.py
@@ -5,3 +5,11 @@ if __name__ == "__main__":
 
     housing = load_housing_data()
     print(housing.head())
+
+    # check for number of observations of each fature, types, nullables, etc.
+    print(housing.info())
+
+    #check non numerical features
+    print(housing["ocean_proximity"].value_counts())
+
+    #check numerical features for scaling, variance, std, mean, outliers, etc.
