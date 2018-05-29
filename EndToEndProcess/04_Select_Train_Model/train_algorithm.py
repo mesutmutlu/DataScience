b717006ec4a3a377bebdd8b90c50a1436de8b690
Mesut MUTLU <mesut_mutlu83@yahoo.fr>
2018-05-29 10:52:10 +0300

    -s

---
+++ EndToEndProcess/04_Select_Train_Model/train_algorithm.py
@@ -0,0 +1,60 @@
+#script number 10
+#with whole training set
+#with whole training set using CV
+#do not forget about ensemble methods (existings or custom created using voting, bagging, pasting, out-of-bag, boosting, stacking, etc.
+from EndToEndProcess.PrepareDataForML.pipeline_data_preparation import housing_preparation_pipeline
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_score
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+import numpy as np
+import sys
+
+
+def train_simple():
+    pass
+
+
+def regression_evaluate_traing_using_cv(lst_algorithms, dataset, labels):
+
+
+    for alg_name, alg_obj, eval_met in lst_algorithms:
+        print(alg_name, alg_obj, eval_met)
+
+        scores = cross_val_score(alg_obj, housing_prepared, housing_labels,
+                                      scoring=eval_met, cv=10)
+        rmse_scores = np.sqrt(-scores)
+
+        print(alg_name)
+        display_scores(rmse_scores)
+
+def display_scores(scores):
+    print("Scores:", scores)
+    print("Mean:", scores.mean())
+    print("Standard deviation:", scores.std())
+
+
+if __name__ == "__main__":
+
+    housing_prepared, housing_labels = housing_preparation_pipeline()
+
+    alg_list= []
+
+    alg_list.append(["tree_reg", DecisionTreeRegressor(), "neg_mean_squared_error"])
+    alg_list.append(["lin_reg", LinearRegression(), "neg_mean_squared_error"])
+    alg_list.append(["for_reg", RandomForestRegressor(),"neg_mean_squared_error"])
+
+    regression_evaluate_traing_using_cv(alg_list, housing_prepared, housing_labels)
+
+    #print(alg_list)
+
+    sys.exit()
+
+
+    tree_reg = DecisionTreeRegressor(LinearRegression(),)
+
+
+    display_scores(rmse_scores)
+
+
+
--- EndToEndProcess/PrepareDataForML/pipeline_data_preparation.py
+++ EndToEndProcess/PrepareDataForML/pipeline_data_preparation.py
@@ -10,9 +10,7 @@ from sklearn.pipeline import FeatureUnion
 from EndToEndProcess.tools.data_frame_selector import DataFrameSelector
 from EndToEndProcess.tools.future_encoders import OneHotEncoder
 
-
-if __name__ == "__main__":
-
+def housing_preparation_pipeline():
     housing = load_housing_data()
 
     housing["income_cat"] = np.ceil(housing["median_income"] / 1.5)
@@ -22,6 +20,8 @@ if __name__ == "__main__":
     print(len(strat_train_set), "strat_train", len(strat_test_set), "strat_test")
 
     housing = strat_train_set
+    housing_labels = strat_train_set["median_house_value"].copy()
+    housing = strat_train_set.drop("median_house_value", axis=1)
     housing_num = housing.drop("ocean_proximity", axis=1)
 
     num_pipeline = Pipeline([
@@ -41,7 +41,7 @@ if __name__ == "__main__":
     ])
     cat_pipeline = Pipeline([
         ('selector', DataFrameSelector(cat_attribs)),
-        ('label_binarizer', OneHotEncoder(sparse=False)),
+        ('one_hot_encoder', OneHotEncoder(sparse=False)),
     ])
     full_pipeline = FeatureUnion(transformer_list=[
         ("num_pipeline", num_pipeline),
@@ -49,4 +49,48 @@ if __name__ == "__main__":
     ])
 
     housing_prepared = full_pipeline.fit_transform(housing)
+    return housing_prepared, housing_labels
+
+
+if __name__ == "__main__":
+
+    housing = load_housing_data()
+
+    housing["income_cat"] = np.ceil(housing["median_income"] / 1.5)
+    housing["income_cat"].where(housing["income_cat"] < 5, 5.0, inplace=True)
+
+    strat_train_set, strat_test_set = stratified_split(housing, 0.2, "income_cat")
+    print(len(strat_train_set), "strat_train", len(strat_test_set), "strat_test")
+
+    housing = strat_train_set
+    housing_labels = strat_train_set["median_house_value"].copy()
+    housing = strat_train_set.drop("median_house_value", axis=1)
+    housing_num = housing.drop("ocean_proximity", axis=1)
+
+    num_pipeline = Pipeline([
+        ('imputer', Imputer(strategy="median")),
+        ('attribs_adder', CombinedAttributesAdder()),
+        ('std_scaler', StandardScaler()),
+    ])
+    housing_num_tr = num_pipeline.fit_transform(housing_num)
+
+    num_attribs = list(housing_num)
+    cat_attribs = ["ocean_proximity"]
+    num_pipeline = Pipeline([
+        ('selector', DataFrameSelector(num_attribs)),
+        ('imputer', Imputer(strategy="median")),
+        ('attribs_adder', CombinedAttributesAdder()),
+        ('std_scaler', StandardScaler()),
+    ])
+    cat_pipeline = Pipeline([
+        ('selector', DataFrameSelector(cat_attribs)),
+        ('one_hot_encoder', OneHotEncoder(sparse=False)),
+    ])
+    full_pipeline = FeatureUnion(transformer_list=[
+        ("num_pipeline", num_pipeline),
+        ("cat_pipeline", cat_pipeline),
+    ])
+
+    housing_prepared = full_pipeline.fit_transform(housing)
+    print(housing.columns)
     print(housing_prepared.shape)
